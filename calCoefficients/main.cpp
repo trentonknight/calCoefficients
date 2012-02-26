8208a33ea033096d8b2cc1062d1f2e722dcd2701
Jason Mansfield <trentonknight@gmail.com>
2012-02-26 18:25:46 -0500

    Final commit

--- calCoefficients/main.cpp
+++ calCoefficients/main.cpp
@@ -6,67 +6,42 @@
 //  Copyright 2012 Regis University. All rights reserved.
 //
 
-//
-//  main.cpp
-//  Binomial
-//
-//  Created by Jason Mansfield on 2/26/12.
-
 
 #include <iostream>
-#include <math.h>
 
-double Factorial(double fact,double lim);
-double GetBinomialCoefficient(double n, double k);
+double pascTri(double num);
 
 using namespace std;
 
 int main ()
 {
-    double n = 1;
-    double k = 0;
-    double binomialCoefficient = 0;
-    
-    while (n > 0) {
-        cout << "Enter object n element for C(n,k): " << endl;
-        cin >> n;
-        cout << "Enter object k-combination for C(n,k): " << endl;
-        cin >> k;
-        binomialCoefficient = GetBinomialCoefficient(n, k);
-        cout << "Binomial Coefficent: " << binomialCoefficient << endl;
+    double coeff = 1; 
+    while(coeff != 0){
+        cout << "PASCALS TRIANGLE" << endl;
+        cout << "Please enter row to view: " << endl;
+        cin >> coeff;
+        if(coeff == 0){
+            cout << "Bye!" << endl;
+        }
+        else{
+            pascTri(coeff);
+        }
     }
-    
-    
-    
-    
     return 0;
 }
-double Factorial(double fact,double lim){
-    double total = 0;
+double pascTri(double num){
     
-    do {
-        total = total + (fact * (fact - 1));
-        fact--;
-    }while((fact - 1) > lim);
-    
-    return total;
-}
-double GetBinomialCoefficient(double n, double k){
-    
-    double lim = n - k;
-    double numerator = n;
-    double denominator = k;
-    double BC = 0;
-    
-    if(k <= 0){
-        return BC = 1;
+    for(int a = 0; a <= num; a++){
+        int b = 1;
+        for(int c = a; c >= 0; c--){
+            cout << b << " ";
+            b = b * c/(a - c + 1);
+        }
+        cout << endl;
     }
-    else{
-        numerator = Factorial(numerator,lim);
-        denominator = Factorial(denominator,0);
-        BC = numerator/denominator;
-        return BC;
-    }
+    
+    
+    return num;
 }
 
 
